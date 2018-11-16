87dbc0482a686f405b767137906998210eb69959
Danilglv <danilglv-a@mail.ru>
2018-11-16 13:11:12 +0700

    add-build

---
+++ build/precache-manifest.ca1841403ad16d0b36d4ee2c04111b83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "77892d91726a33178e7e",
+    "url": "/static/js/main.77892d91.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "77892d91726a33178e7e",
+    "url": "/static/css/main.4ba60159.chunk.css"
+  },
+  {
+    "revision": "63bdef7282470262b4091935a5e63097",
+    "url": "/index.html"
+  }
+];
